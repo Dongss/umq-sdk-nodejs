87162fe83a5dac0d88b8b9e5c6dc3a083cf0d41b
wushukai <wushukainju@gmail.com>
2016-12-20 10:11:47 +0800

    fix typings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 
-import {UmqClient, UmqClientConfig} from './UmqClient';
+import {UmqClient, UmqClientConfig, Producer as _Producer, 
+  Consumer as _Consumer, Subscription as _Subscription, Message as _Message} from './UmqClient';
+import {
+  UmqError as _UmqError,
+  UmqAPIError as _UmqAPIError,
+} from './request';
+
 import * as L from './logger';
 
 export type Config = UmqClientConfig; 
+export type Producer = _Producer;
+export type Consumer = _Consumer;
+export type Subscription = _Subscription;
+export type UmqError = _UmqError;
+export type UmqAPIError = _UmqAPIError;
+export type Message = _Message;
 
 export function newUmqClient(config: UmqClientConfig): UmqClient {
   return new UmqClient({
